ef550d3a08651efb862f02aee14752094b1d975d
mehdilagdimi <lagdimi.mehdi@gmail.com>
2022-02-22 10:12:40 +0100

    exported latest DB file

--- Database/flights_platform.sql
+++ Database/flights_platform.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 19, 2022 at 12:57 PM
+-- Generation Time: Feb 22, 2022 at 10:11 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
@@ -39,6 +39,14 @@ CREATE TABLE `admins` (
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`adminID`, `privilege`, `fName`, `lName`, `phone`, `email`, `birthDate`, `passw`, `createdAt`) VALUES
+(1, '', 'Mehdi', 'Lagdimi', '0606', 'mehdi@gmail.com', '2022-02-20 13:25:21', '1234', '2022-02-20 13:25:21'),
+(2, '', 'ahmed', 'rachid', '0707', 'ahmed@gmail.com', '2022-02-20 13:25:21', '4321', '2022-02-20 13:25:21');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +57,15 @@ CREATE TABLE `airports` (
   `airportID` int(11) NOT NULL,
   `airportAdress` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `airports`
+--
+
+INSERT INTO `airports` (`airportID`, `airportAdress`) VALUES
+(1, 'Al Massira Airport, Agadir'),
+(2, 'Cherif Al Idrissi Airport, Al Hoceima'),
+(3, 'Ben Slimane Airport, Ben Slimane');
 
 -- --------------------------------------------------------
 
@@ -61,6 +78,14 @@ CREATE TABLE `departures` (
   `airportID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `departures`
+--
+
+INSERT INTO `departures` (`volID`, `airportID`) VALUES
+(1, 1),
+(2, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -71,6 +96,14 @@ CREATE TABLE `destinations` (
   `volID` int(11) DEFAULT NULL,
   `airportID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `destinations`
+--
+
+INSERT INTO `destinations` (`volID`, `airportID`) VALUES
+(1, 3),
+(2, 1);
 
 -- --------------------------------------------------------
 
@@ -86,6 +119,14 @@ CREATE TABLE `passengers` (
   `birthDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `passengers`
+--
+
+INSERT INTO `passengers` (`passengerID`, `userID`, `fName`, `lName`, `birthDate`) VALUES
+(1, 1, 'User1', 'User1LASTNAME', '2022-02-21 18:00:33'),
+(2, 2, 'User2', 'User2LASTNAME', '2022-02-21 18:00:33');
+
 -- --------------------------------------------------------
 
 --
@@ -97,6 +138,14 @@ CREATE TABLE `planes` (
   `model` varchar(50) DEFAULT NULL,
   `seats` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `planes`
+--
+
+INSERT INTO `planes` (`planeID`, `model`, `seats`) VALUES
+(1, 'Boeing 737', 700),
+(2, 'Airbus A319', 132);
 
 -- --------------------------------------------------------
 
@@ -113,6 +162,14 @@ CREATE TABLE `reservs` (
   `seatNum` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `reservs`
+--
+
+INSERT INTO `reservs` (`reservID`, `volID`, `passengerID`, `dateReserv`, `goingComing`, `seatNum`) VALUES
+(1, 2, 2, '2022-02-21 18:01:56', 'Going', 4),
+(2, 1, 1, '2022-02-21 18:01:56', 'Going', 6);
+
 -- --------------------------------------------------------
 
 --
@@ -127,6 +184,14 @@ CREATE TABLE `users` (
   `passw` varchar(20) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `phone`, `email`, `birthDate`, `passw`, `createdAt`) VALUES
+(1, '0808', 'user1@gmail.com', '2022-02-20 13:26:23', '0000', '2022-02-20 13:26:23'),
+(2, '0909', 'user2@gmail.com', '2022-02-20 13:26:23', '1111', '2022-02-20 13:26:23');
 
 -- --------------------------------------------------------
 
@@ -144,6 +209,14 @@ CREATE TABLE `vols` (
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `state` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `vols`
+--
+
+INSERT INTO `vols` (`volID`, `planeID`, `departureDate`, `arrivalDate`, `availableSeats`, `price`, `createdAt`, `state`) VALUES
+(1, 1, '2022-02-20 18:55:52', '2022-02-25 18:54:50', 700, 600, '2022-02-20 18:55:52', 'going'),
+(2, 2, '2022-02-20 18:55:52', '2022-02-23 18:54:50', 132, 468.7, '2022-02-20 18:55:52', 'going');
 
 --
 -- Indexes for dumped tables
@@ -217,43 +290,43 @@ ALTER TABLE `vols`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `adminID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `adminID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `airports`
 --
 ALTER TABLE `airports`
-  MODIFY `airportID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `airportID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `passengers`
 --
 ALTER TABLE `passengers`
-  MODIFY `passengerID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `passengerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `planes`
 --
 ALTER TABLE `planes`
-  MODIFY `planeID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `planeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `reservs`
 --
 ALTER TABLE `reservs`
-  MODIFY `reservID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `reservID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `vols`
 --
 ALTER TABLE `vols`
-  MODIFY `volID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `volID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
